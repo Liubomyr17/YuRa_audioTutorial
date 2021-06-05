a0c4454814221735b9fff86c350c4499685fc68a
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-06-05 12:07:39 +0300

    0. Test Commit

---
+++ app/src/main/java/com/lizdepskyi/yura_audiotutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lizdepskyi.yura_audiotutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
