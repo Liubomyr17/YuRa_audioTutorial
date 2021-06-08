e25887749f69623e739b40c3083ca107e4302cc5
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-06-09 00:01:48 +0300

    64. Music Player. Реалізація логіки

--- app/src/main/java/com/lizdepskyi/yura_audiotutorial/MainActivity.java
+++ app/src/main/java/com/lizdepskyi/yura_audiotutorial/MainActivity.java
@@ -8,14 +8,69 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    ImageView playPauseItem;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        playPauseItem = findViewById(R.id.playIconImageView);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);
+    }
+
+    public void previous(View view) {
+    }
+
+    public void next(View view) {
+    }
+
+    public void play(View view) {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            playPauseItem.setImageResource(R.drawable.ic_baseline_play_orange);
+        } else {
+            mediaPlayer.start();
+            playPauseItem.setImageResource(R.drawable.ic_baseline_pause_24);
+        }
     }
 }
